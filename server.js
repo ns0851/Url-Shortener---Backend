fa32e6422fc2c9b50f893daf9fff974202e98f63
satya <ns0851@srmist.edu.in>
2025-05-28 01:46:31 +0530

    Changed URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://quick-short-mu.vercel.app",
     credentials: true
 }))
 app.use(cookieParser());
